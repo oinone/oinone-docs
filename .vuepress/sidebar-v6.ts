17b5e3ddccbdfb6e91c06a6e96d9bceea52b788a
zbh <zbh@shushi.pro>
2026-04-01 14:04:41 +0800

    fix: sidebar

---
+++ .vuepress/sidebar-v6.ts
@@ -0,0 +1,99 @@
+export const zhCNSidebarConfig = [
+    {
+        text: "产品特性说明",
+        link: "Description-Of-Product-Characteristics",
+        icon: "/assets/icon/产品特性说明.svg",
+    },
+    {
+        text: "安装与升级",
+        link: "InstallOrUpgrade/",
+        prefix: "InstallOrUpgrade/",
+        collapsible: true,
+        icon: "/assets/icon/安装与升级.svg",
+        children: "structure",
+    },
+    {
+        text: "用户手册",
+        link: "UserManual/",
+        prefix: "UserManual/",
+        collapsible: true,
+        icon: "/assets/icon/用户手册.svg",
+        children: "structure",
+    },
+    {
+        text: "研发手册",
+        link: "DevManual/",
+        prefix: "DevManual/",
+        collapsible: true,
+        icon: "/assets/icon/研发手册.svg",
+        children: "structure",
+    },
+    {
+        text: "贡献手册",
+        link: "Contribute/",
+        prefix: "Contribute/",
+        collapsible: true,
+        icon: "/assets/icon/贡献手册.svg",
+        children: "structure",
+    },
+    {
+        text: "第三方开源软件及许可说明",
+        icon: "/assets/icon/第三方开源软件及许可说明备份.svg",
+        link: "Third-Party-Open-Source-Software-And-License-Notice",
+    },
+    {
+        text: "软件使用许可和合约",
+        icon: "/assets/icon/软件使用许可和合约.svg",
+        link: "software-licenses",
+    },
+]
+
+export const enSidebarConfig = [
+    {
+        text: "Description Of Product Characteristics",
+        link: "Description-Of-Product-Characteristics",
+        icon: "/assets/icon/产品特性说明.svg",
+    },
+    {
+        text: "Install Or Upgrade",
+        link: "InstallOrUpgrade/",
+        prefix: "InstallOrUpgrade/",
+        collapsible: true,
+        icon: "/assets/icon/安装与升级.svg",
+        children: "structure",
+    },
+    {
+        text: "UserManual",
+        link: "UserManual/",
+        prefix: "UserManual/",
+        collapsible: true,
+        icon: "/assets/icon/用户手册.svg",
+        children: "structure",
+    },
+    {
+        text: "DevManual",
+        link: "DevManual/",
+        prefix: "DevManual/",
+        collapsible: true,
+        icon: "/assets/icon/研发手册.svg",
+        children: "structure",
+    },
+    {
+        text: "Contribute",
+        link: "Contribute/",
+        prefix: "Contribute/",
+        collapsible: true,
+        icon: "/assets/icon/贡献手册.svg",
+        children: "structure",
+    },
+    {
+        text: "Third Party Open Source Software And License Notice",
+        icon: "/assets/icon/第三方开源软件及许可说明备份.svg",
+        link: "Third-Party-Open-Source-Software-And-License-Notice",
+    },
+    {
+        text: "Software Licenses",
+        icon: "/assets/icon/软件使用许可和合约.svg",
+        link: "software-licenses",
+    },
+]
--- .vuepress/sidebar.ts
+++ .vuepress/sidebar.ts
@@ -1,110 +1,101 @@
-import { sidebar, SidebarOptions } from "vuepress-theme-hope"
+import { sidebar } from 'vuepress-theme-hope';
+import { enSidebarConfig as en_V6, zhCNSidebarConfig as zh_V6 } from './sidebar-v6';
 
 const enSidebarConfig = [
   {
-    text: "Description Of Product Characteristics",
-    link: "Description-Of-Product-Characteristics",
-    icon: "/assets/icon/产品特性说明.svg",
-  },
-  {
-    text: "Install Or Upgrade",
-    link: "InstallOrUpgrade/",
-    prefix: "InstallOrUpgrade/",
+    text: 'Install Or Upgrade',
+    link: 'InstallOrUpgrade/',
+    prefix: 'InstallOrUpgrade/',
     collapsible: true,
-    icon: "/assets/icon/安装与升级.svg",
-    children: "structure",
+    icon: '/assets/icon/安装与升级.svg',
+    children: 'structure'
   },
   {
-    text: "UserManual",
-    link: "UserManual/",
-    prefix: "UserManual/",
+    text: 'UserManual',
+    link: 'UserManual/',
+    prefix: 'UserManual/',
     collapsible: true,
-    icon: "/assets/icon/用户手册.svg",
-    children: "structure",
+    icon: '/assets/icon/用户手册.svg',
+    children: 'structure'
   },
   {
-    text: "DevManual",
-    link: "DevManual/",
-    prefix: "DevManual/",
+    text: 'DevManual',
+    link: 'DevManual/',
+    prefix: 'DevManual/',
     collapsible: true,
-    icon: "/assets/icon/研发手册.svg",
-    children: "structure",
+    icon: '/assets/icon/研发手册.svg',
+    children: 'structure'
   },
   {
-    text: "Contribute",
-    link: "Contribute/",
-    prefix: "Contribute/",
+    text: 'Contribute',
+    link: 'Contribute/',
+    prefix: 'Contribute/',
     collapsible: true,
-    icon: "/assets/icon/贡献手册.svg",
-    children: "structure",
+    icon: '/assets/icon/贡献手册.svg',
+    children: 'structure'
   },
   {
-    text: "Third Party Open Source Software And License Notice",
-    icon: "/assets/icon/第三方开源软件及许可说明备份.svg",
-    link: "Third-Party-Open-Source-Software-And-License-Notice",
+    text: 'Third Party Open Source Software And License Notice',
+    icon: '/assets/icon/第三方开源软件及许可说明备份.svg',
+    link: 'Third-Party-Open-Source-Software-And-License-Notice'
   },
   {
-    text: "Software Licenses",
-    icon: "/assets/icon/软件使用许可和合约.svg",
-    link: "software-licenses",
-  },
-]
+    text: 'Software Licenses',
+    icon: '/assets/icon/软件使用许可和合约.svg',
+    link: 'software-licenses'
+  }
+];
 
 const zhCNSidebarConfig = [
   {
-    text: "产品特性说明",
-    link: "Description-Of-Product-Characteristics",
-    icon: "/assets/icon/产品特性说明.svg",
-  },
-  {
-    text: "安装与升级",
-    link: "InstallOrUpgrade/",
-    prefix: "InstallOrUpgrade/",
+    text: '安装与升级',
+    link: 'InstallOrUpgrade/',
+    prefix: 'InstallOrUpgrade/',
     collapsible: true,
-    icon: "/assets/icon/安装与升级.svg",
-    children: "structure",
+    icon: '/assets/icon/安装与升级.svg',
+    children: 'structure'
   },
   {
-    text: "用户手册",
-    link: "UserManual/",
-    prefix: "UserManual/",
+    text: '用户手册',
+    link: 'UserManual/',
+    prefix: 'UserManual/',
     collapsible: true,
-    icon: "/assets/icon/用户手册.svg",
-    children: "structure",
+    icon: '/assets/icon/用户手册.svg',
+    children: 'structure'
   },
   {
-    text: "研发手册",
-    link: "DevManual/",
-    prefix: "DevManual/",
+    text: '研发手册',
+    link: 'DevManual/',
+    prefix: 'DevManual/',
     collapsible: true,
-    icon: "/assets/icon/研发手册.svg",
-    children: "structure",
+    icon: '/assets/icon/研发手册.svg',
+    children: 'structure'
   },
   {
-    text: "贡献手册",
-    link: "Contribute/",
-    prefix: "Contribute/",
+    text: '贡献手册',
+    link: 'Contribute/',
+    prefix: 'Contribute/',
     collapsible: true,
-    icon: "/assets/icon/贡献手册.svg",
-    children: "structure",
+    icon: '/assets/icon/贡献手册.svg',
+    children: 'structure'
   },
   {
-    text: "第三方开源软件及许可说明",
-    icon: "/assets/icon/第三方开源软件及许可说明备份.svg",
-    link: "Third-Party-Open-Source-Software-And-License-Notice",
+    text: '第三方开源软件及许可说明',
+    icon: '/assets/icon/第三方开源软件及许可说明备份.svg',
+    link: 'Third-Party-Open-Source-Software-And-License-Notice'
   },
   {
-    text: "软件使用许可和合约",
-    icon: "/assets/icon/软件使用许可和合约.svg",
-    link: "software-licenses",
-  },
-]
+    text: '软件使用许可和合约',
+    icon: '/assets/icon/软件使用许可和合约.svg',
+    link: 'software-licenses'
+  }
+];
 
 export const sidebarConfig = sidebar({
-  "/en/": enSidebarConfig,
-  "/zh-cn/": zhCNSidebarConfig,
-  "/v6/en/": enSidebarConfig,
-  "/v6/zh-cn/": zhCNSidebarConfig,
-})
+  '/en/': enSidebarConfig,
+  '/zh-cn/': zhCNSidebarConfig,
+  '/v6/en/': en_V6,
+  '/v6/zh-cn/': zh_V6
+});
 
 // export default sidebar(options)
